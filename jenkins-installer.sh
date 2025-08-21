b1e542041b3f63578ce95ed89f946fd8337d9095
devwalex <salamiusmanolawale@gmail.com>
2025-08-21 23:13:19 +0100

    chore: update docker agent

--- jenkins-installer.sh
+++ jenkins-installer.sh
@@ -4,4 +4,15 @@ apt update
 
 apt install docker.io
 
-docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 --restart=on-failure jenkins/jenkins:lts
+docker run -d -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -u root  -p 8080:8080 -p 50000:50000 --restart=on-failure jenkins/jenkins:lts
+
+
+#install docker on aws
+
+sudo yum update
+
+sudo yum install docker
+
+sudo service docker start
+
+sudo usermod -aG docker $USER
