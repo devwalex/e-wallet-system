766b02d2b813b7904ce0b246939ade82f6f70524
devwalex <salamiusmanolawale@gmail.com>
2025-08-25 19:08:30 +0100

    chore: update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -71,10 +71,7 @@ module.exports = {
       user:     process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      ssl: {
-        rejectUnauthorized: true,
-      }
+      port: process.env.DB_PORT
     },
     pool: {
       min: 2,
